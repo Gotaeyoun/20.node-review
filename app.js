f2806dca7eb6ecefeb1c3e58deb20bd6552f7977
Gotaeyoun <ohdin486@gmail.com>
2020-07-28 11:59:32 +0900

    crud 완성

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
 
+/**************** 내부 모듈 ******************/
+const { pool, mysql } = require('./modules/mysql-mod');
+
 /**************** 서버 실행 ******************/
 app.listen(process.env.PORT, () => { 
 	console.log(`http://127.0.0.1:${process.env.PORT}`) 
 });
 
-/**************** 내부 모듈 ******************/
-
-
 /**************** 경로 설정 ******************/
 const publicPath = path.join(__dirname, './public');
-console.log(publicPath);
+const viewsPath = path.join(__dirname, './views');
 
 /**************** 초기 설정 ******************/
-app.use(express.json()); //json 파싱
-app.use(express.urlencoded({extended: false})); 
-app.use("/", express.static(publicPath)); 
+app.set('view engine', 'pug');
+app.set('views', viewsPath);
+app.locals.pretty = true;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use("/", express.static(publicPath));
 
 /**************** 라우터 설정 ******************/
+app.use("/crud", require('./routers/crud-router'));
 
-/**************** 오류 설정 ******************/
+
+
